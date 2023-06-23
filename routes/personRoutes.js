eba3a61ea5dc810e2edd7670146a59098bc282ab
davisouzal <45435035+davisouzal@users.noreply.github.com>
2023-06-23 11:10:17 -0300

    finalizado o crud
    atualizado o erro de verificação do getbyID e implementada as outra rotas de delete e update (nesse caso patch)

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -60,24 +60,102 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
 
     const id = req.params.id
-    
-    try{
-        
-        const person = await Person.findOne({_id: id})
-        
-        console.log(person)
 
-        if(!person){
-            res.status(422).json({ message: "Usuário não encontrado"})
+    if(id.length!=24){
+        res.status(422).json({ message: "O número de caracteres de um usuário é de 24 digitos. Por favor verifique se você o digitou corretamente."})
+    }
+    else{
+        try{
+            
+            const person = await Person.findOne({_id: id})
+            
+            console.log("Pessoa: " + person)
+
+            if(!person){
+                res.status(422).json({ message: "Usuário não encontrado"})
+                return
+            }
+
+            res.status(200).json(person)
+
+        }catch(error){
+            res.status(500).json({error: error})
+        }
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    if(id.length!=24){
+        res.status(422).json({ message: "O número de caracteres de um usuário é de 24 digitos. Por favor verifique se você o digitou corretamente."})
+    }
+    else{
+        const { name, salary, boolean } = req.body
+
+        if(!name){
+            res.status(422).json({ error: "O nome é obrigatório!"})
+            return
+        }
+        if(!salary){
+            res.status(422).json({ error: "O salário é obrigatório!"})
+            return
+        }
+        if(!boolean){
+            res.status(422).json({ error: "O status de aprovação é obrigatório!"})
             return
         }
 
-        res.status(200).json(person)
+        const approved = ( boolean === "true")
 
-    }catch(error){
+        const person = {
+            name,
+            salary,
+            approved,
+        }
+
+        try{
+
+            const updatePerson = await Person.updateOne({_id: id}, person)
+        
+            res.status(200).json(person)
+
+        }catch(error){
         res.status(500).json({error: error})
-    }
+        }
 
+    }
 })
+
+router.delete('/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    if(id.length!=24){
+        res.status(422).json({ message: "O número de caracteres de um usuário é de 24 digitos. Por favor verifique se você o digitou corretamente."})
+    }
+    else{
+        
+        const person = await Person.findOne({_id: id})
+
+        if(!person){
+            res.status(422).json({ message: 'O usuário não foi encontrado! '})
+            return
+        }
+        
+        try {
+            
+            await Person.deleteOne({_id: id})
+            
+            res.status(200).json({message: "Usuário removido com sucesso!"})
+
+        }catch(error){
+            res.status(500).json({ error: error})
+        }
+    }
+})
+
+
 
 module.exports = router
